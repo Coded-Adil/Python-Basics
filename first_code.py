c5318c86110833a61757c93578597f11f3750c34
Coded-Adil <itsadil2022@gmail.com>
2026-01-12 21:31:57 +0600

    first commit

---
+++ first_code.py
@@ -0,0 +1,1 @@
+print("Hello, World! time to code.")
---
+++ string.py
@@ -0,0 +1,18 @@
+# dynamically typed string variable
+
+my_string = "This is a string variable"
+
+print(my_string)
+
+print(type(my_string))
+
+"""
+This is a multiline comment
+"""
+
+new_string = '''This is another way
+to define a string variable'''
+
+print(new_string)
+
+print(type(new_string))
