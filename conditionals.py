14a32460c7b08eb81005a5f329411e109677265b
Coded-Adil <itsadil2022@gmail.com>
2026-01-16 22:23:20 +0600

    Conditional Statement if else basics-1

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+# Conditional Statements in Python
+# if else, elif
+
+rain = input("Is it raining? (1 for yes, 0 for no): ")
+rain = int(rain) 
+
+if rain == 1:
+    print("Take an umbrella")
+else:
+    print("No need for an umbrella")
