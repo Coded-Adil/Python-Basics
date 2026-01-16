bb80c102dd7a530086e010dc6e23aece3ab6b506
Coded-Adil <itsadil2022@gmail.com>
2026-01-16 22:13:40 +0600

    math in-build functions like floor round and ceil

---
+++ math_functions.py
@@ -0,0 +1,19 @@
+import math
+# 1. Ceil --> Closest greater integer
+x = 4.5 # 5
+y = 4.1 # 1
+
+print(math.ceil(x))
+print(math.ceil(y))
+
+# 2. Floor --> Closest smaller integer
+
+print(math.floor(x))
+print(math.floor(y))
+
+# 3. Round --> Nearest integer
+print(round(x))
+print(round(y))
+
+z = 4.56789
+print(round(z, 2)) # Rounding to 2 decimal places
