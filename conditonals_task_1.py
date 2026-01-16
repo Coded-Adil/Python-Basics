f572f31e19f836f9e31258359fdfb026a93fa0c0
Coded-Adil <itsadil2022@gmail.com>
2026-01-16 22:29:59 +0600

    Conditional Statement if elif else basics-2 with task-1

---
+++ conditonals_task_1.py
@@ -0,0 +1,10 @@
+# Check if a number is positive, negative, or zero
+
+num = int(input("Enter a number:"))
+
+if num > 0:
+    print("The {} is positive.".format(num))
+elif num == 0:
+    print("The {} is zero.".format(num))
+else:
+    print("The {} is negative.".format(num))
