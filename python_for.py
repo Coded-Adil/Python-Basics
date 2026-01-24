28db539b0ec86f7a0aa29b2b557140cf309664ae
Coded-Adil <itsadil2022@gmail.com>
2026-01-24 22:40:27 +0600

    For loop in python

---
+++ python_for.py
@@ -0,0 +1,18 @@
+# 1 - 1000
+
+a = [1, 2, 3, 4]
+
+# b = "Hello"
+
+for item in a:
+    print(item)
+
+# print("a" in "Hello")
+# print("e" in "Hello")
+
+for i in range(1, 1001):
+    print(i)
+
+for i in range(101) :
+    print(f"Nasa is hacking - {i}%")
+print("Nasa is hacked successfully")
