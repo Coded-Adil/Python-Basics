36c8d64ff8bcc20e76db48222b0135c678e49c4b
Coded-Adil <itsadil2022@gmail.com>
2026-01-27 13:26:21 +0600

    Functions in python, built-in functions in python

---
+++ Function, File Handling & Error Handling/function.py
@@ -0,0 +1,17 @@
+# Functions -> 2 Types
+# ---------------------------
+# 1. Built-in Functions -> available in Python by default => print(), len(), type()
+# 2. User-defined Functions -> created by the user
+# ---------------------------
+
+print("Hello, World!") # Doesn't return any value
+user_name = input("Enter your name: ") # Returns a value
+print(f"Hello, {user_name}!")
+
+
+# ------------------------------------------------------------------
+
+print("-------------------------------------------")
+
+mx = max([1, 2, 3, 4])
+print(f"Max value: {mx}. {mx * 3}")
