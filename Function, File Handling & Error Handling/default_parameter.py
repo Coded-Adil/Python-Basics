4433eb607a8b9ec3d58365f39161d615783469cc
Coded-Adil <itsadil2022@gmail.com>
2026-01-28 17:01:54 +0600

    Using default parameters in python

---
+++ Function, File Handling & Error Handling/default_parameter.py
@@ -0,0 +1,8 @@
+def print_my_name(f_name, l_name="Rahman"): # l_name has a default value
+    print(f"My name is {f_name} {l_name}.")
+
+print_my_name("Adil")
+print_my_name("Salman", "Khan") # Overriding the default value
+
+def greetings():
+    pass
