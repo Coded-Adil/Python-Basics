445fa1b6315018b765a1cc674d018933c8c0899b
Coded-Adil <itsadil2022@gmail.com>
2026-01-30 14:27:27 +0600

    Writing, appending and appending multiple lines to a txt file named name.txt with file_handling.py using python

--- Function, File Handling & Error Handling/file_handling.py
+++ Function, File Handling & Error Handling/file_handling.py
@@ -3,6 +3,37 @@
 # print(content)
 # file.close()
 
+# Nahidul Islam Prince
+# Keranigonj, Dhaka
+
+
+# Abu Bakkar Siddiq Sazzad
+# Uttara, Dhaka
+
+
+# Raisul Islam Rifat
+# Gazipur, Dhaka
+
+
+# Adil Rahman Srijon
+# Mirpur 10, Dhaka
+
+with open('name.txt', 'r') as f:
+    content = f.read()
+    print(content)
+
+# with open('name.txt', 'w') as f:
+#     f.write("VoVo Squad \n")  # Overwrites the file
+#     f.write("Writing in the file using Python \n")
+
+# Append mode
+with open('name.txt', 'a') as f:
+    f.write("\nWriting in the file using Python \n")
+
+lines = ['I am learning Python.\n', 'File handling is easy.\n', 'This is the third line.\n']
+with open('name.txt', 'a') as f:
+    f.writelines(lines)
+
 with open('name.txt', 'r') as f:
     content = f.read()
     print(content)
